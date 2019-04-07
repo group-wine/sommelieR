df3eb251401e78c56e576534e7108ac6e4fd7f6a
Bryce Rowland <bryce.rowland@unc.edu>
2019-04-07 16:57:04 -0400

    change functions to work better with optim. optim doesn't think the gradient function is correct (and it's probably right).

--- R/multinomial_likelihood.R
+++ R/multinomial_likelihood.R
@@ -30,7 +30,7 @@ data_to_mulitnomial <- function(data, formula, intercept = T){
   return(list(multi_y, X))
 }
 
-multinomial_log_likelihood <- function(y, X, betas){
+multinomial_log_likelihood <- function(y, X, b_vector){
   #This function expects:
 
   #y should be an (n x (k-1)) matrix for the dependent variable as created by the
@@ -38,18 +38,37 @@ multinomial_log_likelihood <- function(y, X, betas){
 
   #X should be an (n x p) matrix, where p is the number of predictors
 
-  #beta should be a (p x (k - 1)) matrix.
+  #b_vector should be a (p*(k-1) x 1) vector because the optim function expects the
+  #parameters on which we optimize to be in a vector. However, the function converts
+  #this vector to a (p x (k-1)) vector initially for ease of programming.
+  beta <- matrix(b_vector, ncol = ncol(y))
+  if(nrow(y)  != nrow(X)){
+    stop("Make sure that X and y have the same number of rows")
+  }
 
-  n <- nrow(y)
-  l <- c(rep(0, n))
-  for(i in 1:n){
+  if(ncol(y) != ncol(beta)){
+    stop("Make sure y and beta have the same number of columns")
+  }
+
+  if(ncol(X) != nrow(beta)){
+    stop("Make sure that X and beta have conformable columns and rows")
+  }
+
+
+  N <- nrow(y)
+  l <- c(rep(0, N))
+
+  for(i in 1:N){
+    t1 <- (X[i,] %*% beta)[which(y[2,] == 1)]
+    t2 <- log(1 + sum(exp(X[i,] %*% beta)))
     l[i] <- (X[i,] %*% beta)[which(y[2,] == 1)] - log(1 + sum(exp(X[i,] %*% beta)))
   }
   l_n <- sum(l)
   return(l_n)
 }
 
-mll_gradient <- function(y, X, betas){
+mll_gradient <- function(y, X, b_vector){
+
   #This function expects:
 
   #y should be an (n x (k-1)) matrix for the dependent variable as created by the
@@ -57,10 +76,46 @@ mll_gradient <- function(y, X, betas){
 
   #X should be an (n x p) matrix, where p is the number of predictors
 
-  #beta should be a (p x (k - 1)) matrix.
+  #b_vector should be a (p*(k-1) x 1) vector because the optim function expects the
+  #parameters on which we optimize to be in a vector. However, the function converts
+  #this vector to a (p x (k-1)) vector initially for ease of programming.
 
- n <- nrow(y)
- g <- matrix(rep(0, ncol(betas)))
+  beta <- matrix(b_vector, ncol = ncol(y))
+  if(nrow(y)  != nrow(X)){
+    stop("Make sure that X and y have the same number of rows")
+  }
+
+  if(ncol(y) != ncol(beta)){
+    stop("Make sure y and beta have the same number of columns")
+  }
+
+  if(ncol(X) != nrow(beta)){
+    stop("Make sure that X and beta have conformable columns and rows")
+  }
+
+  #Will return a (p*(k -1) x 1) vector.
+
+  n <- nrow(y)
+  p <- nrow(beta)
+  k_minus_one <- ncol(beta)
+  g <- matrix(rep(0, p*k_minus_one), ncol = k_minus_one)
+  print(c(p, k_minus_one))
+  #Itterate down the row in each column of the beta matrix. Each row corresponds to
+  #the parameter associated with each covariate in the model.
+  for(i in 1:p){
+    #For each column in the new beta matrix corresponding to each level of the dependent
+    #variable.
+    for(j in 1:k_minus_one){
+      element <- rep(0, n)
+      #Iterate over all subjects since they all contribute to each value of the gradient.
+      for (k in 1:n){
+        element[k] <- y[k,j]*X[k,][i] + (exp(X[k,] %*% beta[,j])*X[k,][i])/(1 + sum(exp(X[j,] %*% beta)))
+      }
+      g[i,j] <- sum(element)
+    }
+  }
+  #Convert gradient back to (p*(k - 1) x 1) vector and return it.
+  return(matrix(g, ncol = 1))
 }
 
 
