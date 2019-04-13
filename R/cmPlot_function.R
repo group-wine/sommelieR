c0bab1b3f49c9ad1c917aff9f17f2dc8d8a9fc35
Taylor Lagler <tmlagler@Taylors-MacBook-Pro-5.local>
2019-04-13 13:41:26 -0400

    adds documentation

--- R/cmPlot_function.R
+++ R/cmPlot_function.R
@@ -1,33 +1,19 @@
-#-----------------------------------------------------------------------------#
-
-# Confusion Matrix Plotting Function
-
-#-----------------------------------------------------------------------------#
-
-# cm is confusion matrix formatted as a dataframe
-# 0 counts must be changed to NA
-# 3 columns, third is frequency counts
-# 1st and 2nd cols are pred/reference categories
-##### class.red <- as.data.frame(cM.red$table)
-##### class.red$Freq[class.red$Freq == 0] <-NA
-
-# red_or_white colors the plot accordingly
-# enter "red" or "white" (in quotes)
-
-# pred_first specificies first columns contains the prediction
-# category and the second column contains the reference
-# set to FALSE if columns reversed
-
-# title is the plot title (in quotes)
-
-# see classify_by_quality.R for examples
-# make sure to source the file to use the function
-#### source("./R/cmPlot_function.R") 
-
-#-----------------------------------------------------------------------------#
-
-library(ggplot2)
-
+#' Visualize a Confusion Matrix (ggplot)
+#'
+#' Given a dataframe and a formula object, will return the necessary dependent
+#' variable matrix and the model matrix needed to run the optimization for
+#' multinomial regression for a given reference level.
+#'
+#' @param cm a 3 column dataframe with columns for prediction and reference categroies and corresponding frequency counts (third column). 0 counts should be converted to NA
+#' @param red_or_white character string c("red", "white") to specify plot coloring
+#' @param pred_first logical, whether first column contains the prediction category (default is TRUE)
+#' @param title character string to be used as plot title
+#'
+#' @return a ggplot object visualizing the confusion matrix of reference versus predicted 
+#'
+#' @import ggplot2
+#'
+#' @export
 cmPlot <- function(cm, red_or_white, pred_first = TRUE, title){
   
   if(red_or_white == "red"){
@@ -45,11 +31,10 @@ cmPlot <- function(cm, red_or_white, pred_first = TRUE, title){
   colnames(cm) <- c("Prediction", "Reference", "Freq")
   
   cmPlot <- ggplot(cm, aes(x = Reference, y = Prediction, size = Freq, fill=Freq, label=Freq)) +
-  scale_size(range=c(2,20)) + geom_label(na.rm = T) + theme_minimal() +
-  scale_fill_continuous(low=lowcol, high=highcol) + guides(size=FALSE) +
-  ggtitle(title) + theme(plot.title = element_text(hjust=.5, size=20))
+    scale_size(range=c(2,20)) + geom_label(na.rm = T) + theme_minimal() +
+    scale_fill_continuous(low=lowcol, high=highcol) + guides(size=FALSE) +
+    ggtitle(title) + theme(plot.title = element_text(hjust=.5, size=20))
 
   return(cmPlot)
 }
 
-#-----------------------------------------------------------------------------#
